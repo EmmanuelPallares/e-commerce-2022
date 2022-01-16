50924446d10dadd241c82efa5f5c2c7acfab5978
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-01-15 23:42:17 -0600

    basic routing and install react router dom VERSION 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
 import "./App.css";
 
+const Example = () => (
+  <>
+    <h1>Example</h1>
+  </>
+);
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/example" component={Example} />
+        </Switch>
       </div>
     );
   }
