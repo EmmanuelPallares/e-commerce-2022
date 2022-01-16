ecffc8a36bb3e4f8d3abeb5e0161079f0d54757b
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-01-15 18:08:38 -0600

    home-page-categories-banners

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
 
-function App() {
-  return (
-    <div >
-      Hola mundo
-    </div>
-  );
+import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,15 @@
+import "./homepage.styles.scss";
+
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="Home-Page">
+        <Directory />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
