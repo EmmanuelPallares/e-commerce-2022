015b389840d6c7eb67b0c07964a56dc5d55374df
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-01-21 11:53:04 -0600

    update the menus with the withRouter high order component for best navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import HatsPage from "./pages/hatspage/hatspage.component";
 import "./App.css";
 
 const Example = () => (
@@ -15,6 +16,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/example" component={Example} />
+          <Route path="/hats" component={HatsPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/hatspage/hatspage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hello world hats page</h1>
+    </div>
+  );
+};
+
+export default HatsPage;
